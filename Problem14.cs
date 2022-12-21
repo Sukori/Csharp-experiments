e8aa90c1c20bc286a30a1064a2e11ef69878f6a3
Sukori <sukori98@gmail.com>
2022-12-21 18:06:51 +0100

    Problem 14 finished

--- Problem14.cs
+++ Problem14.cs
@@ -10,7 +10,50 @@ namespace ProjectEuler
     {
         public static void LongestCollatz()
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<int, int> chains = new();
+
+            int countSteps;
+            long result; //result may go far above 1 000 000 :) #neverforget
+
+           for(int i = 1; i < 1000000; i++)
+           {
+                result = i;
+                countSteps = 0;
+
+                while (result > 1)
+                {
+                    //ne pas calculer jusqu'au bout si on tombe sur une key déjà vue. ça fait gagner beaucoup de temps.
+                    if (chains.ContainsKey((int)result))
+                    {
+                        countSteps += chains[(int)result];
+                        break;
+                    }
+
+                    if (result % 2 == 0)
+                    {
+                        result /= 2;
+                    }
+                    else
+                    {
+                        result = (3 * result + 1);
+                    }
+                    countSteps++;
+                }
+                chains.Add(i, countSteps);
+            }
+
+            int maxNumber = 0;
+            int maxValue = 0;
+
+            foreach (var kvp in chains)
+            {
+                if (kvp.Value > maxValue)
+                {
+                    maxNumber = kvp.Key;
+                    maxValue = kvp.Value;
+                }
+            }
+            Console.WriteLine("Le nombre qui a la plus grande chaine est " + maxNumber + " avec " + maxValue + " pas.");
         }
     }
 }
